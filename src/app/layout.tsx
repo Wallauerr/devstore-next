cf5031dfc7941e4387e9eda8cdc2023965338204
Wallauerr <wallauer@protonmail.com>
2024-03-06 19:20:38 -0300

    adjust font inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+const inter = Inter({
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: {
@@ -17,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html className={inter.variable} lang="pt">
+    <html className={inter.className} lang="pt">
       <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   )
